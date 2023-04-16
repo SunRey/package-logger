b4cbd9bac1d636e4fc13b520414fefe68818b168
Jonathan Reyes <jreyes.satx@gmail.com>
2023-04-16 01:37:35 -0500

    navigation links to package view for desktop and responsive

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('packages.index')" :active="route().current('packages.index')">
+                                    Packages
+                                </NavLink>
                             </div>
                         </div>
 
@@ -114,6 +117,9 @@ const showingNavigationDropdown = ref(false);
                     <div class="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('packages.index')" :active="route().current('packages.index')">
+                            Packages
                         </ResponsiveNavLink>
                     </div>
 

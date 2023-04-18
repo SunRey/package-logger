ca0d2f206620a3468c6cf624aac3af31af8d75a6
Jonathan Reyes <jreyes.satx@gmail.com>
2023-04-17 23:52:28 -0500

    add Person controller, form resources, Model, and migration. Update packags migration for correct creation order of tabels

---
+++ database/migrations/2023_04_17_194923_create_persons_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName')->nullable('false');
+            $table->string('lastName')->nullable('true');
+            $table->string('group')->nullable('true');
+            $table->string('email')->nullable('ture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('persons');
+    }
+};
---
+++ database/migrations/2023_04_17_194934_create_packages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('tracking')->nullable('false');
+            $table->foreignId('person_id')->constrained();
+            $table->date('deliverDate')->default(Carbon::now());
+            $table->date('pickupDate')->nullable('true');
+            $table->boolean('delivered')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};

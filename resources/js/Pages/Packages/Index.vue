6c90d6592fa9721e95c6360408e70e2be5c32dbb
Jonathan Reyes <jreyes.satx@gmail.com>
2023-05-06 23:38:58 -0500

    update form name fields to use camelCase convention, add props for packages data, and add Link tags for Show view on each Person. Add id field as fillable on form

--- resources/js/Pages/Packages/Index.vue
+++ resources/js/Pages/Packages/Index.vue
@@ -5,25 +5,46 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { useForm, Head } from '@inertiajs/vue3';
 
 const form = useForm({
-    package: '',
-    person: '',
+    tracking: '',
+    persons_id: '',
 });
+
+defineProps(['packages']);
 </script>
 
 <template>
     <Head title="Packages" />
 
     <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Packages</h2>
+        </template>
+
         <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
             <form @submit.prevent="form.post(route('packages.store'), { onSuccess: () => form.reset() })">
                 <input
-                    v-model="form.package"
+                    v-model="form.tracking"
                     placeholder="Enter Tracking"
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                 />
-                <InputError :message="form.errors.package" class.mt-2 />
+                <InputError :message="form.errors.tracking" class.mt-2 />
+                <input
+                    v-model="form.persons_id"
+                    placeholder="Person Here"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                />
+                <InputError :message="form.errors.persons_id" class.mt-2 />
                 <PrimaryButton class="mt-4">Confirm</PrimaryButton>
             </form>
+        </div>
+        <div class="max-w-2x3 mx-auto p-4 sm:p-6 lg:p-8">
+            {{ packages }}
+            <div class="flex justify-between items-center"
+                 v-for="shipment in packages">
+                <p class="mt-4 text-lg text-gray-900">
+                    {{ shipment.tracking}} belongs to
+                </p>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Persons/Index.vue
+++ resources/js/Pages/Persons/Index.vue
@@ -3,14 +3,16 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { useForm, Head } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
+import NavLink from "@/Components/NavLink.vue";
 
 const form = useForm({
-    first_name: '',
-    last_name: '',
+    firstName: '',
+    lastName: '',
     group: '',
 });
 
-defineProps(['persons']);
+defineProps(['persons', 'packages']);
 </script>
 
 <template>
@@ -25,17 +27,17 @@ defineProps(['persons']);
 
             <form @submit.prevent="form.post(route('persons.store'), { onSuccess: () => form.reset() })">
                 <input
-                    v-model="form.first_name"
+                    v-model="form.firstName"
                     placeholder="Enter First Name"
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                 />
-                <InputError :message="form.errors.first_name" class.mt-2 />
+                <InputError :message="form.errors.firstName" class.mt-2 />
                 <input
-                    v-model="form.last_name"
+                    v-model="form.lastName"
                     placeholder="Enter Last Name"
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                 />
-                <InputError :message="form.errors.last_name" class.mt-2 />
+                <InputError :message="form.errors.lastName" class.mt-2 />
                 <input
                     v-model="form.group"
                     placeholder="Enter Event or Group"
@@ -48,10 +50,11 @@ defineProps(['persons']);
         <div class="max-w-2x3 mx-auto p-4 sm:p-6 lg:p-8">
             <div class="flex justify-between items-center"
                  v-for="person in persons">
-                <p class="mt-4 text-lg text-gray-900">
-                    {{ person.first_name }} {{ person.last_name}} with {{ person.group}}
-                </p>
+                <Link :href="'/persons/' + person.id" class="mt-4 text-lg text-gray-900">
+                    {{ person.first_name }} {{ person.las_name}} with {{ person.group}}
+                </Link>
             </div>
+            {{ packages }}
         </div>
     </AuthenticatedLayout>
 </template>
